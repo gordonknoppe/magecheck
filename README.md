b41f5acc5acaac093cb1d810bb24ce33cc8ac98d
Gordon Knoppe <gknop@guidance.com>
2012-04-17 23:34:46 -0700

    Initial project commit

---
+++ README.md
@@ -0,0 +1,35 @@
+Magento Health Check
+====================
+
+Magento health check is a php script that analyzes your environment against a list of Magento requirements and best practices.
+
+What does it do?
+-----------
+
+* Apache requirements and optimizations
+* PHP requirements and optimizations
+* MySQL optimizations (if Magento is installed)
+* Magento optimizations (if Magento is installed)
+
+How to use it
+-----------
+
+Copy the script to your server root.  If Magento is detected and installed additional information about MySQL and Magento will be detected.
+
+License
+-----------
+
+MIT License:
+
+Copyright (c) 2012 Gordon Knoppe
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+Acknowledgements
+------------
+
+Magento Health Check is a project by [Gordon Knoppe](http://github.com/gknoppe-guidance).
---
+++ magecheck.php
@@ -0,0 +1,186 @@
+<?php
+/**
+ * Magento health check
+ *
+ * Script used to analyze an environment hosting Magento
+ * @author Gordon Knoppe
+ */
+
+
+function magecheck_result($test, $success, $failure, $recommended = false)
+{
+    $class = "notice";
+    if ($test) {
+        $class .= " success";
+        return sprintf('<li class="%s">%s</li>', $class, $success) . PHP_EOL;
+    } else {
+        if (!$recommended) {
+            $class .= " error-message";
+        }
+        return sprintf('<li class="%s">%s</li>', $class, $failure) . PHP_EOL;
+    }
+}
+
+// Load phpinfo for apache vars not exposed via php functions
+ob_start();
+phpinfo();
+$phpinfo = ob_get_contents();
+ob_end_clean();
+
+?>
+<html>
+<head>
+    <title><?php echo $_SERVER['HTTP_HOST'];?> - Magento Check</title>
+    <style type="text/css">/*! normalize.css 2012-03-11T12:53 UTC - http://github.com/necolas/normalize.css */article,aside,details,figcaption,figure,footer,header,hgroup,nav,section,summary{display:block}audio,canvas,video{display:inline-block;*display:inline;*zoom:1}audio:not([controls]){display:none;height:0}[hidden]{display:none}html{font-size:100%;-webkit-text-size-adjust:100%;-ms-text-size-adjust:100%}html,button,input,select,textarea{font-family:sans-serif}body{margin:0}a:focus{outline:thin dotted}a:hover,a:active{outline:0}h1{font-size:2em;margin:.67em 0}h2{font-size:1.5em;margin:.83em 0}h3{font-size:1.17em;margin:1em 0}h4{font-size:1em;margin:1.33em 0}h5{font-size:.83em;margin:1.67em 0}h6{font-size:.75em;margin:2.33em 0}abbr[title]{border-bottom:1px dotted}b,strong{font-weight:bold}blockquote{margin:1em 40px}dfn{font-style:italic}mark{background:#ff0;color:#000}p,pre{margin:1em 0}pre,code,kbd,samp{font-family:monospace,serif;_font-family:'courier new',monospace;font-size:1em}pre{white-space:pre;white-space:pre-wrap;word-wrap:break-word}q{quotes:none}q:before,q:after{content:'';content:none}small{font-size:75%}sub,sup{font-size:75%;line-height:0;position:relative;vertical-align:baseline}sup{top:-0.5em}sub{bottom:-0.25em}dl,menu,ol,ul{margin:1em 0}dd{margin:0 0 0 40px}menu,ol,ul{padding:0 0 0 40px}nav ul,nav ol{list-style:none;list-style-image:none}img{border:0;-ms-interpolation-mode:bicubic}svg:not(:root){overflow:hidden}figure{margin:0}form{margin:0}fieldset{border:1px solid #c0c0c0;margin:0 2px;padding:.35em .625em .75em}legend{border:0;padding:0;white-space:normal;*margin-left:-7px}button,input,select,textarea{font-size:100%;margin:0;vertical-align:baseline;*vertical-align:middle}button,input{line-height:normal}button,input[type="button"],input[type="reset"],input[type="submit"]{cursor:pointer;-webkit-appearance:button;*overflow:visible}button[disabled],input[disabled]{cursor:default}input[type="checkbox"],input[type="radio"]{box-sizing:border-box;padding:0;*height:13px;*width:13px}input[type="search"]{-webkit-appearance:textfield;-moz-box-sizing:content-box;-webkit-box-sizing:content-box;box-sizing:content-box}input[type="search"]::-webkit-search-decoration,input[type="search"]::-webkit-search-cancel-button{-webkit-appearance:none}button::-moz-focus-inner,input::-moz-focus-inner{border:0;padding:0}textarea{overflow:auto;vertical-align:top}table{border-collapse:collapse;border-spacing:0}</style>
+    <style type="text/css">
+            /** Notices and Errors **/
+        .message {
+            clear: both;
+            color: #fff;
+            font-size: 140%;
+            font-weight: bold;
+            margin: 0 0 1em 0;
+            padding: 5px;
+        }
+
+        .success,
+        .message,
+        .cake-error,
+        .cake-debug,
+        .notice,
+        p.error,
+        .error-message {
+            background: #ffcc00;
+            background-repeat: repeat-x;
+            background-image: -moz-linear-gradient(top, #ffcc00, #E6B800);
+            background-image: -ms-linear-gradient(top, #ffcc00, #E6B800);
+            background-image: -webkit-gradient(linear, left top, left bottom, from(#ffcc00), to(#E6B800));
+            background-image: -webkit-linear-gradient(top, #ffcc00, #E6B800);
+            background-image: -o-linear-gradient(top, #ffcc00, #E6B800);
+            background-image: linear-gradient(top, #ffcc00, #E6B800);
+            text-shadow: 0 -1px 0 rgba(0, 0, 0, 0.25);
+            border: 1px solid rgba(0, 0, 0, 0.2);
+            margin-bottom: 18px;
+            padding: 7px 14px;
+            color: #404040;
+            text-shadow: 0 1px 0 rgba(255, 255, 255, 0.5);
+            -webkit-border-radius: 4px;
+            -moz-border-radius: 4px;
+            border-radius: 4px;
+            -webkit-box-shadow: inset 0 1px 0 rgba(255, 255, 255, 0.25);
+            -moz-box-shadow: inset 0 1px 0 rgba(255, 255, 255, 0.25);
+            box-shadow: inset 0 1px 0 rgba(255, 255, 255, 0.25);
+        }
+
+        .success,
+        .message,
+        .cake-error,
+        p.error,
+        .error-message {
+            clear: both;
+            color: #fff;
+            background: #c43c35;
+            border: 1px solid rgba(0, 0, 0, 0.5);
+            background-repeat: repeat-x;
+            background-image: -moz-linear-gradient(top, #ee5f5b, #c43c35);
+            background-image: -ms-linear-gradient(top, #ee5f5b, #c43c35);
+            background-image: -webkit-gradient(linear, left top, left bottom, from(#ee5f5b), to(#c43c35));
+            background-image: -webkit-linear-gradient(top, #ee5f5b, #c43c35);
+            background-image: -o-linear-gradient(top, #ee5f5b, #c43c35);
+            background-image: linear-gradient(top, #ee5f5b, #c43c35);
+            text-shadow: 0 -1px 0 rgba(0, 0, 0, 0.3);
+        }
+
+        .success {
+            clear: both;
+            color: #fff;
+            border: 1px solid rgba(0, 0, 0, 0.5);
+            background: #3B8230;
+            background-repeat: repeat-x;
+            background-image: -webkit-gradient(linear, left top, left bottom, from(#76BF6B), to(#3B8230));
+            background-image: -webkit-linear-gradient(top, #76BF6B, #3B8230);
+            background-image: -moz-linear-gradient(top, #76BF6B, #3B8230);
+            background-image: -ms-linear-gradient(top, #76BF6B, #3B8230);
+            background-image: -o-linear-gradient(top, #76BF6B, #3B8230);
+            background-image: linear-gradient(top, #76BF6B, #3B8230);
+            text-shadow: 0 -1px 0 rgba(0, 0, 0, 0.3);
+        }
+
+        p.error {
+            font-family: Monaco, Consolas, Courier, monospace;
+            font-size: 120%;
+            padding: 0.8em;
+            margin: 1em 0;
+        }
+
+        p.error em {
+            font-weight: normal;
+            line-height: 140%;
+        }
+
+        .notice {
+            color: #000;
+            display: block;
+            font-size: 90%;
+            margin: 1em 0;
+        }
+
+        .success {
+            color: #fff;
+        }
+    </style>
+</head>
+<body>
+
+<h1>Magento Health Check</h1>
+
+<ul>
+    <?php echo magecheck_result(true, "The following statement is true", "The prior statement was false"); ?>
+    <?php echo magecheck_result(false, "The following statement is true", "The prior statement was false"); ?>
+</ul>
+
+<h2>Apache</h2>
+
+<ul>
+    <?php echo magecheck_result(strpos($phpinfo, 'Keep Alive: off'), "Keep Alive is not enabled", "Keep Alive is enabled"); ?>
+</ul>
+
+<h2>PHP</h2>
+
+<ul>
+    <?php echo magecheck_result(version_compare(PHP_VERSION, '5.2.13') >= 0, "PHP ".PHP_VERSION." is installed", "PHP 5.2.13 or higher is required"); ?>
+</ul>
+
+<?php $extensions = get_loaded_extensions(); ?>
+<ul>
+    <?php $required_extensions = array('curl', 'dom', 'gd', 'hash', 'iconv', 'mcrypt', 'mhash', 'PDO', 'pdo_mysql', 'SimpleXML', 'soap'); ?>
+    <?php foreach ($required_extensions as $extension): ?>
+        <?php echo magecheck_result(in_array($extension, $extensions), "Extension $extension is installed", "Extension $extension is required"); ?>
+    <?php endforeach; ?>
+    <?php $recommended_extensions = array('apc', 'memcache'); ?>
+    <?php foreach ($recommended_extensions as $extension): ?>
+        <?php echo magecheck_result(in_array($extension, $extensions), "Extension $extension is installed", "Extension $extension is recommended", true); ?>
+    <?php endforeach; ?>
+</ul>
+
+<h3>PHP APC Settings</h3>
+
+<?php if (in_array('apc', $extensions)): ?>
+<ul>
+    <?php echo magecheck_result(ini_get('apc.shm_size') >= 256, "apc.shm_size is " . ini_get('apc.shm_size'), "apc.shm_size should be 256 or higher"); ?>
+    <?php echo magecheck_result(ini_get('apc.num_files_hint') >= 10000, "apc.num_files_hint is " . ini_get('apc.num_files_hint'), "apc.num_files_hint should be 10000 or higher"); ?>
+    <?php echo magecheck_result(ini_get('apc.user_entries_hint') >= 10000, "apc.user_entries_hint is " . ini_get('apc.user_entries_hint'), "apc.user_entries_hint should be 10000 or higher"); ?>
+    <?php echo magecheck_result(ini_get('apc.max_file_size') >= 5, "apc.max_file_size is " . ini_get('apc.max_file_size'), "apc.max_file_size should be 5M or higher"); ?>
+</ul>
+<?php else: ?>
+<ul>
+    <?php echo magecheck_result(false, "", "APC not detected", true); ?>
+</ul>
+<?php endif; ?>
+
+<h2>Magento</h2>
+
+<h2>MySQL</h2>
+
+</body>
+</html>
